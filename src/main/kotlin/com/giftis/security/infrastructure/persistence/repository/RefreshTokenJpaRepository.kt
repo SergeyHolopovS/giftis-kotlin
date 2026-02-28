d22e768c06bd44c5d63ba5882569a6fb84fca72e
SergeyHolopov <gssdsudas@gmail.com>
2026-02-28 15:53:35 +0300

    fix: маленькие фиксы валидации

--- src/main/kotlin/com/giftis/security/infrastructure/persistence/repository/RefreshTokenJpaRepository.kt
+++ src/main/kotlin/com/giftis/security/infrastructure/persistence/repository/RefreshTokenJpaRepository.kt
@@ -3,6 +3,7 @@ package com.giftis.security.infrastructure.persistence.repository
 import com.giftis.security.infrastructure.persistence.entity.RefreshTokenJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 import java.util.*
 
@@ -17,6 +18,7 @@ interface RefreshTokenJpaRepository : JpaRepository<RefreshTokenJpaEntity, UUID>
 
     fun deleteByToken(token: String)
 
+    @Transactional
     fun deleteByExpiresAtBefore(dateTime: Instant?)
 
 }
--- src/main/kotlin/com/giftis/user/infrastructure/web/requests/RegistrationRequest.kt
+++ src/main/kotlin/com/giftis/user/infrastructure/web/requests/RegistrationRequest.kt
@@ -1,5 +1,8 @@
 package com.giftis.user.infrastructure.web.requests
 
+import jakarta.validation.constraints.NotBlank
+
 data class RegistrationRequest(
+    @NotBlank
     val initData: String,
 )
